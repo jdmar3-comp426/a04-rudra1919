5cd1ca218afe8b8565346734097263ff1b3d7170
rudra19@live.unc.edu <Rudy*1906$>
2021-11-28 18:56:05 -0500

    json edit

--- server.js
+++ server.js
@@ -38,7 +38,8 @@ app.get("/app/user/:id", (req, res) => {
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {	
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id =  " + req.params.id).all();
-	res.status(200).json(stmt);
+	res.json({"message":"1 record updated: ID " + req.params.id + " (200)"})
+	res.status(200);
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 
